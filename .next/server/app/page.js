b8ca0f7b0aafb1661be96d023b3928a67256c94a
Adrian <bluepop11@hotmail.com>
2023-09-19 14:56:41 -0500

    fix: remove standalone

--- .next/server/app/page.js
+++ .next/server/app/page.js
@@ -212,7 +212,7 @@ module.exports = require("url");
 
 /***/ }),
 
-/***/ 6858:
+/***/ 481:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
@@ -301,21 +301,14 @@ const routeModule = new AppPageRouteModule({
 
 /***/ }),
 
-/***/ 1040:
-/***/ (() => {
-
-
-
-/***/ }),
-
-/***/ 1843:
+/***/ 8313:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 1232, 23));
-Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 6926, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 2987, 23));
-Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 4282, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 831, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 6926, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 4282, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 6505, 23))
 
 /***/ }),
@@ -324,6 +317,13 @@ Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 3380, 23))
+
+/***/ }),
+
+/***/ 5467:
+/***/ (() => {
+
+
 
 /***/ }),
 
@@ -573,7 +573,7 @@ __webpack_require__.r(__webpack_exports__);
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [587,81], () => (__webpack_exec__(6858)));
+var __webpack_exports__ = __webpack_require__.X(0, [587,81], () => (__webpack_exec__(481)));
 module.exports = __webpack_exports__;
 
 })();
