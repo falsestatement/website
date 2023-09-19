cda8baad3e03958b27987f437baf46afa5603701
Adrian <bluepop11@hotmail.com>
2023-09-19 14:41:17 -0500

    fix: .next directory

--- .next/server/app/page.js
+++ .next/server/app/page.js
@@ -301,19 +301,12 @@ const routeModule = new AppPageRouteModule({
 
 /***/ }),
 
-/***/ 1040:
-/***/ (() => {
-
-
-
-/***/ }),
-
-/***/ 7:
+/***/ 8313:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 1232, 23));
-Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 831, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 2987, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 831, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 6926, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 4282, 23));
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 6505, 23))
@@ -324,6 +317,13 @@ Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
 Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 3380, 23))
+
+/***/ }),
+
+/***/ 5467:
+/***/ (() => {
+
+
 
 /***/ }),
 
