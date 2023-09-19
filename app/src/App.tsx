fe236fbdfb79685d6436384394950464c1442760
Adrian <bluepop11@hotmail.com>
2023-09-19 16:47:16 -0500

    feat: routes test

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to='/'>OpenLab</Link>
+                        </li>
+                        <li>
+                            <Link to='/about'>About</Link>
+                        </li>
+                        <li>
+                            <Link to='/active_projects'>Active Projects</Link>
+                        </li>
+                        <li>
+                            <Link to='/safety'>Safety</Link>
+                        </li>
+                        <li>
+                            <Link to='/contact'>Contact Us</Link>
+                        </li>
+                        <li>
+                            <Link to='/facilities'>Facilities</Link>
+                        </li>
+                        <li>
+                            <Link to='/resources'>Resources</Link>
+                        </li>
+                    </ul>
+
+                    <Routes>
+                        <Route path='/'>Home</Route>
+                        <Route path='/about'>About</Route>
+                        <Route path='/active_projects'>Active Projects</Route>
+                        <Route path='/safety'>Safety</Route>
+                        <Route path='/contact'>Contact Us</Route>
+                        <Route path='/facilities'>Facilities</Route>
+                        <Route path='/resources'>Resources</Route>
+                    </Routes>
+
+                </nav>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
