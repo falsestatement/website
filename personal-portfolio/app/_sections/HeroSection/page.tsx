5b983644d38a5bff5505fc51d146a7b2fda50390
Adrian Cheng <bluepop11@hotmail.com>
2024-05-28 10:50:43 -0400

    fix: responsive issues with hero section

--- personal-portfolio/app/_sections/HeroSection/page.tsx
+++ personal-portfolio/app/_sections/HeroSection/page.tsx
@@ -6,23 +6,24 @@ const HeroSection = () => {
   return (
     <section className={styles.heroSection}>
       <Image
-        width={1755}
-        height={1170}
+        width={1200}
+        height={800}
         src="/abstract-cpu-5.png"
         style={{ objectFit: "cover" }}
         className={styles.image}
         alt="Abstract CPU Image"
+                priority
       />
       <div className={styles.title}>
         <h1 className={styles.mainHeading}>ADRIAN CHENG</h1>
         <h5 className={styles.subHeading}>
           Developing the whole technology stack, all the way down to hardware.
         </h5>
-        <button className={styles.resumeButton}>
-          <DownloadIcon />
-          Resume
-        </button>
       </div>
+      <button className={styles.resumeButton}>
+        <DownloadIcon />
+        Resume
+      </button>
     </section>
   );
 };
