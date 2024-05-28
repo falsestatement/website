89ac3b50d4ce4e527ae43c60ef402fa9aa0af709
Adrian Cheng <bluepop11@hotmail.com>
2024-05-28 00:41:45 -0400

    feat: responsive about section

--- personal-portfolio/app/AboutSection/page.tsx
+++ personal-portfolio/app/AboutSection/page.tsx
@@ -1,5 +1,6 @@
 import styles from "./page.module.css";
 import TechSkillIcon from "@icon/TechSkillIcon";
+import Image from "next/image";
 const AboutSection = () => {
   const techSkills = [
     "c",
@@ -30,21 +31,31 @@ const AboutSection = () => {
 
   return (
     <section className={styles["about-section"]}>
-      <div className={styles.photo}>Professional Photo Here</div>
-      <h4 className={styles.description}>
-        A master’s student with interest in RTL design, programming, 3D
-        modeling, and finance.
-      </h4>
-            <div className={styles.skills}>
-                <h6 className={styles["skills-subheading"]}>Technical Skills</h6>
-                <ul className={styles["skills-list"]}>
-                    {techSkills.map((skill) => (
-                        <li key={`${skill}-icon`}>
-                            <TechSkillIcon variant={skill} />
-                        </li>
-                    ))}
-                </ul>
-            </div>
+      <div className={styles.photo}>
+        <Image src="/donotcommit/test.jpg" fill style={{objectFit: "cover", objectPosition: "50% 20%"}} alt="headshot.jpg" />
+      </div>
+      <div className={styles.description}>
+        <p className={styles["description-short"]}>
+          A Master’s student with passion in RTL design, programming, 3D
+          modeling, and finance.
+        </p>
+        <p className={styles["description-long"]}>
+          Hello! I am a master’s student attending the University of Illinois,
+          Urbana Champaign, studying financial engineering. I have a wide range
+          of interests, ranging from hardware layout and RTL design, all the way
+          to web development, UI/UX design, and much more!
+        </p>
+      </div>
+      <div className={styles.skills}>
+        <h6 className={styles["skills-subheading"]}>Technical Skills</h6>
+        <ul className={styles["skills-list"]}>
+          {techSkills.map((skill) => (
+            <li key={`${skill}-icon`}>
+              <TechSkillIcon variant={skill} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
