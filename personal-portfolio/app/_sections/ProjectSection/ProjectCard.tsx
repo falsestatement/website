1e4390109ebf5601f23775f8be3433705c3e54f1
Adrian Cheng <bluepop11@hotmail.com>
2024-05-29 13:26:05 -0400

    feat: hover effects

--- personal-portfolio/app/_sections/ProjectSection/ProjectCard.tsx
+++ personal-portfolio/app/_sections/ProjectSection/ProjectCard.tsx
@@ -3,42 +3,43 @@ import { ProjectType } from "@type/ProjectType";
 import Image from "next/image";
 
 const ProjectCard = ({
-  className,
-  project,
-  bodyStyle,
+    className,
+    project,
+    bodyStyle,
 }: {
-  className?: string;
-  project: ProjectType;
-  bodyStyle?: string;
+    className?: string;
+    project: ProjectType;
+    bodyStyle?: string;
 }) => {
-  return (
-    <article className={[styles.card, className].join(" ")}>
-      <Image
-        src={`/projects/${project.img}`}
-        width={1080}
-        height={1080}
-        style={{
-          objectFit: "cover",
-        }}
-        alt={project.img}
-        className={styles.image}
-      />
-      <div className={[styles["image-filter"], bodyStyle].join(" ")} />
-      <div className={[styles.main, bodyStyle].join(" ")}>
-        <div>
-          <h6 className={styles.title}>{project.title}</h6>
-          <p className={styles.description}>{project.description}</p>
-        </div>
-        <div className={styles.tags}>
-          {project.tags.map((tag) => (
-            <div className={styles.tag} key={`${tag}-tag`}>
-              {tag}
+    return (
+        <article className={[styles.card, className].join(" ")}>
+            <Image
+                src={`/projects/${project.img}`}
+                width={1080}
+                height={1080}
+                style={{
+                    objectFit: "cover",
+                    objectPosition: "left",
+                }}
+                alt={project.img}
+                className={styles.image}
+            />
+            <div className={[styles["image-filter"], bodyStyle].join(" ")} />
+            <div className={[styles.main, bodyStyle].join(" ")}>
+                <div>
+                    <h6 className={styles.title}>{project.title}</h6>
+                    <p className={styles.description}>{project.description}</p>
+                </div>
+                <div className={styles.tags}>
+                    {project.tags.map((tag) => (
+                        <div className={styles.tag} key={`${tag}-tag`}>
+                            {tag}
+                        </div>
+                    ))}
+                </div>
             </div>
-          ))}
-        </div>
-      </div>
-    </article>
-  );
+        </article>
+    );
 };
 
 export default ProjectCard;
