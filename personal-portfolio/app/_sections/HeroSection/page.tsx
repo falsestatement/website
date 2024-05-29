6db733a2ef0ce8da8fb186d684a9dfbcc0511c3a
Adrian Cheng <bluepop11@hotmail.com>
2024-05-29 14:36:10 -0400

    revert: hero section nuke

--- personal-portfolio/app/_sections/HeroSection/page.tsx
+++ personal-portfolio/app/_sections/HeroSection/page.tsx
@@ -5,15 +5,6 @@ import styles from "./page.module.css";
 const HeroSection = () => {
   return (
     <section className={styles.heroSection}>
-      <Image
-        width={1200}
-        height={800}
-        src="/abstract-cpu-5.png"
-        style={{ objectFit: "cover" }}
-        className={styles.image}
-        alt="Abstract CPU Image"
-        priority
-      />
       <div className={styles.title}>
         <h1 className={styles.mainHeading}>ADRIAN CHENG</h1>
         <h5 className={styles.subHeading}>
