5f3f7e5ac1b3c9b5a1f73c8d2c3b708a479a4245
Adrian Cheng <bluepop11@hotmail.com>
2025-01-26 17:53:11 -0600

    feat: added pegatron to experience

--- app/_sections/ExperienceSection/page.tsx
+++ app/_sections/ExperienceSection/page.tsx
@@ -4,44 +4,50 @@ import CircuitRight from "./CircuitRight";
 import { forwardRef, useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-import ScrollTrigger from "gsap/ScrollTrigger";
 
 const ExperienceSection = forwardRef<HTMLElement>((_, ref) => {
   const experienceHistory = [
     {
-      startDate: "May 2024",
-      endDate: "Jan 2024",
+      endDate: "Aug 2024",
+      startDate: "Jun 2024",
+      organization: "Pegatron Corporation",
+      position: "Artificial Intelligence / Machine Learning Lead Intern",
+      description: "Managed a team of 9 interns in the development of an automatic issue tracking system. Utilizing a custom in house LLM agent to automatically sort, manage, and generate statistics on a variety of issue tracking systems. The issue tracking assistant has placed within the top 5 within a company-wide AI innovation contest.",
+    },
+    {
+      endDate: "May 2024",
+      startDate: "Jan 2024",
       organization: "ROC Military",
       position: "Infantry Trainee",
       description: "4 month conscription within Taiwan.",
     },
     {
-      startDate: "Dec 2023",
-      endDate: "Dec 2022",
+      endDate: "Dec 2023",
+      startDate: "Dec 2022",
       organization: "John Deere Financial",
       position: "Intern Project Lead",
       description:
         "Lead a team of 6 through the development and deployment of a internal management application, allowing managers to more easily manage the access of their employees, saving over 37,000 USD worth of man hours in waiting for access changes to take effect.",
     },
     {
-      startDate: "Dec 2022",
-      endDate: "Aug 2022",
+      endDate: "Dec 2022",
+      startDate: "Aug 2022",
       organization: "John Deere Financial",
       position: "Full Stack Engineering Intern",
       description:
         "Added automatic incident ticket grouping through the use of ML as well as a frontend interface for our client to manage the groupings, decreasing the time spent on manual sorting and grouping from several days to minutes.",
     },
     {
-      startDate: "May 2022",
-      endDate: "Sep 2021",
+      endDate: "May 2022",
+      startDate: "Sep 2021",
       organization: "Grainger College of Engineering",
       position: "Multimedia Assistant",
       description:
         "Filmed and edited official college promotional and informational videos using professional filming equipment as well as editing in both Adobe Premiere Pro and After Effects.",
     },
     {
-      startDate: "May 2021",
-      endDate: "Jan 2021",
+      endDate: "May 2021",
+      startDate: "Jan 2021",
       organization: "Grainger College of Engineering",
       position: "Engineering Course Assistant",
       description:
@@ -103,9 +109,9 @@ const ExperienceSection = forwardRef<HTMLElement>((_, ref) => {
             className={styles.job}
           >
             <div className={[styles.date, "date-container"].join(" ")}>
-              <h6 className={styles.start}>{job.startDate.toUpperCase()}</h6>
+              <h6 className={styles.start}>{job.endDate.toUpperCase()}</h6>
               <div className={styles["date-line"]} />
-              <h6 className={styles.end}>{job.endDate.toUpperCase()}</h6>
+              <h6 className={styles.end}>{job.startDate.toUpperCase()}</h6>
             </div>
             <div
               className={[styles.content, "job-content-container"].join(" ")}
